4b3af03f635aadbb2537d2a0418b5cbce821e828
XenderDq <120644975+XenderDq@users.noreply.github.com>
2025-10-14 20:19:49 +0300

    Update template.php

--- BitrixEx/bitrix/local/templates/my_template/components/bitrix/news.list/main_news/template.php
+++ BitrixEx/bitrix/local/templates/my_template/components/bitrix/news.list/main_news/template.php
@@ -1,3 +1,15 @@
-<a href="<?=$arResult['ITEMS'][0]['LIST_PAGE_URL']?>">
-    Ссылка на новости
-</a>
+<?php foreach ($arResult['ITEMS'] as $item): ?>
+    <a class="article-item article-list__item" href="for-individuals.html" data-anim="anim-3">
+        <div class="article-item__background">
+            <img src="<?=$item['PREVIEW_PICTURE']['SRC'] ?? ''?>"
+                 data-src="<?=$item['DETAIL_PICTURE']['SRC'] ?? ''?>"
+                 alt="<?=$item['NAME']?>"/>
+        </div>
+        <div class="article-item__wrapper">
+            <div class="article-item__title"><?=$item['NAME']?></div>
+            <div class="article-item__content">
+                <?= strip_tags($item['PREVIEW_TEXT']) ?>
+            </div>
+        </div>
+    </a>
+<?php endforeach; ?>
