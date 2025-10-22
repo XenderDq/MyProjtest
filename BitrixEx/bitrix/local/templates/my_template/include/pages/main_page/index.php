c023535457011a3e911c60239f3cf5a669563d14
XenderDq <120644975+XenderDq@users.noreply.github.com>
2025-10-22 13:06:06 +0300

    Update main page

--- BitrixEx/bitrix/local/templates/my_template/include/pages/main_page/index.php
+++ BitrixEx/bitrix/local/templates/my_template/include/pages/main_page/index.php
@@ -62,3 +62,26 @@
       
   </div>
 </div>
+<div class="brr" style="margin-top: 50px;"></div>
+<?$APPLICATION->IncludeComponent(
+    "bitrix:form.result.new",
+    "testform",
+    array(
+        "WEB_FORM_ID" => "2", 
+        "IGNORE_CUSTOM_TEMPLATE" => "N",
+        "USE_EXTENDED_ERRORS" => "Y",
+        "SEF_MODE" => "N",
+        "CACHE_TYPE" => "A",
+        "CACHE_TIME" => "3600",
+        "LIST_URL" => "",
+        "EDIT_URL" => "",
+        "SUCCESS_URL" => "",
+        "CHAIN_ITEM_TEXT" => "",
+        "CHAIN_ITEM_LINK" => "",
+        "VARIABLE_ALIASES" => array(
+            "WEB_FORM_ID" => "WEB_FORM_ID",
+            "RESULT_ID" => "RESULT_ID"
+        )
+    ),
+    false
+);?>
