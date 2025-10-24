4b2b81ec96ffd9db4cd7e3a964dc10d715258935
XenderDq <120644975+XenderDq@users.noreply.github.com>
2025-10-24 13:00:08 +0300

    Update template.php

--- BitrixEx/bitrix/local/templates/my_template/components/bitrix/form.result.new/testform/template.php
+++ BitrixEx/bitrix/local/templates/my_template/components/bitrix/form.result.new/testform/template.php
@@ -21,54 +21,34 @@
     <?=$arResult["FORM_HEADER"]?>
 
     <div class="contact-form__form">
-        
         <div class="contact-form__form-inputs">
-            <?php foreach ($arResult['arQuestions'] as $key => $value):?>
+            <?foreach ($arResult['QUESTIONS'] as $key => $value):?>
                 <div class="input contact-form__input">
-                    <?php if ($value['TITLE']==='Email'):?> 
-                        <label class="input__label" for="form_email_<?=$value['ID']?>">
-                    <?php endif;?>
-                    <?php if ($value['TITLE']!='Email'):?> 
-                        <label class="input__label" for="form_text_<?=$value['ID']?>">
-                    <?php endif;?>
-                        <div class="input__label-text"><?=$value['TITLE']?>*</div>
-                    <?php if ($value['TITLE']!='Email' && $value['TITLE']!='Номер телефона'):?>
-                        <input class="input__input" type="text" id="form_text_<?=$value['ID']?>" name="form_text_<?=$value['ID']?>" value="<?=htmlspecialchars($arResult['arrVALUES']['form_text_' . $value['ID']] ?? '')?>" required>
-                    <?php endif;?>
-                    <?php if ($value['TITLE']==='Email'):?>
-                        <input class="input__input" type="email" id="form_email_<?=$value['ID']?>" name="form_email_<?=$value['ID']?>" value="<?=htmlspecialchars($arResult['arrVALUES']['form_email_' . $value['ID']] ?? '')?>" required>
-                    <?php endif;?>
-                    <?php if ($value['TITLE']==='Номер телефона'):?>
-                        <input class="input__input" type="tel" id="form_text_<?=$value['ID']?>" name="form_text_<?=$value['ID']?>" value="<?=htmlspecialchars($arResult['arrVALUES']['form_text_' . $value['ID']] ?? '')?>" 
-                            data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'" maxlength="12" x-autocompletetype="phone-full" required>
-                    <?php endif;?>
-                    <?php if ($value['TITLE']!='Сообщение' && $value['TITLE']!='Номер телефона'):?>
+                    <label class="input__label">
+                        <div class="input__label-text"><?=$value['CAPTION']?><?if($value['REQUIRED'] == 'Y'):?>*<?endif;?></div>
+                        <?=$arResult['QUESTIONS'][$key]['HTML_CODE']?>
                         <div class="input__notification"><?=$value['COMMENTS']?></div>
-                    <?php endif;?>
                     </label>
                 </div>
             <?php endforeach;?>
-        </div>
-        
+
+        <!-- Поле: Сообщение -->
         <div class="contact-form__form-message">
             <div class="input">
-                <label class="input__label" for="form_textarea_<?=$arResult['LAST_ELEM']['ID']?>">
-                    <div class="input__label-text"><?=$arResult['LAST_ELEM']['TITLE']?></div>
-                    <textarea class="input__input" id="form_textarea_<?=$arResult['LAST_ELEM']['ID']?>" name="form_textarea_<?=$arResult['LAST_ELEM']['ID']?>"><?=htmlspecialchars($arResult['arrVALUES']['form_text_' . $arResult['LAST_ELEM']['ID']] ?? '')?></textarea>
+                <label class="input__label">
+                    <div class="input__label-text"><?=$arResult['LAST_ELEM']['CAPTION']?></div>
+                    <?=$arResult['LAST_ELEM']['HTML_CODE']?>
                     <div class="input__notification"></div>
                 </label>
             </div>
         </div>
 
         <div class="contact-form__bottom">
-            <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что
-                ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных данных&raquo;.
-            </div>
+            <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных данных&raquo;.</div>
             <button class="form-button contact-form__bottom-button" type="submit" name="web_form_submit" data-success="Отправлено" data-error="Ошибка отправки">
-                <div class="form-button__title"><?=$arResult['arForm']['BUTTON'];?></div>
+                <div class="form-button__title"><?=$arResult["SUBMIT_BUTTON"]?></div>
             </button>
         </div>
-
     </div>
 
     <?=$arResult["FORM_FOOTER"]?>
