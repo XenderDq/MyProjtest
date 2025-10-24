f2f09df1a41f8f3976e606cdbf054fbfa79ffd0f
XenderDq <120644975+XenderDq@users.noreply.github.com>
2025-10-24 13:00:19 +0300

    Update result_modifier.php

--- BitrixEx/bitrix/local/templates/my_template/components/bitrix/form.result.new/testform/result_modifier.php
+++ BitrixEx/bitrix/local/templates/my_template/components/bitrix/form.result.new/testform/result_modifier.php
@@ -1,3 +1,11 @@
 <?php
-$arResult['LAST_ELEM'] = array_pop($arResult['arQuestions']);
+$commAdd = []; 
+
+ foreach ($arResult['arQuestions'] as $key => $value) {
+    $commAdd[$key] = $value['COMMENTS']; 
+ }
+ foreach ($commAdd as $key => $value) {
+    $arResult['QUESTIONS'][$key]['COMMENTS'] = $value;
+ }
+ $arResult['LAST_ELEM'] = array_pop($arResult['QUESTIONS']);
 ?>
