5535c0cbf31b581543bd89847cbce2872e02baef
XenderDq <120644975+XenderDq@users.noreply.github.com>
2025-10-27 16:59:03 +0300

    Update urlrewrite.php

--- BitrixEx/urlrewrite.php
+++ BitrixEx/urlrewrite.php
@@ -25,3 +25,11 @@ $arUrlRewrite=array (
     'SORT' => 100,
   ),
 );
+6 => 
+  array (
+    'CONDITION' => '#^/novosti/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/novosti/index.php',
+    'SORT' => 100,
+  ),
